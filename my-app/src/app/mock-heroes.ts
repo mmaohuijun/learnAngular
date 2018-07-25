818cedc13698dda3827aa31a5354af75d115dae7
mmaohuijun <1462019088@qq.com>
2018-07-25 23:12:22 +0800

    have a try

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero.ts'
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'mr.rice' },
+  { id: 12, name: 'rice' },
+  { id: 13, name: 'jack' },
+  { id: 14, name: 'mike' },
+  { id: 15, name: 'helen' },
+  { id: 16, name: 'barry' },
+  { id: 17, name: 'john' },
+  { id: 18, name: 'tom' },
+  { id: 19, name: 'eric' }
+];
